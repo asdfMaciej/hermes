36f3e423e3eab8423ae36c4080afca911da95f3d
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2020-08-31 01:49:03 +0200

    begin work on deploy scripts

---
+++ deploy/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /root/hermes
+git pull
+cp -rf /root/hermes/app/controllers/* /var/www/hermes/controllers
+cp -rf /root/hermes/app/models/* /var/www/hermes/models
+cp -rf /root/hermes/app/static/* /var/www/hermes/static
+cp -rf /root/hermes/app/views/* /var/www/hermes/views
+
+mv -f /var/www/hermes/application/config.php /root/hermes/old_config.php
+cp -rf /root/hermes/app/application/* /var/www/hermes/application
+mv -f /root/hermes/old_config.php /var/www/hermes/application/config.php
