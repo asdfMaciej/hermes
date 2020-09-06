faea715b8fdb06fbf3311b42c41e7c4e8bc006b9
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2020-09-06 15:27:32 +0200

    add google analytics tag

--- app/views/skeleton/header.php
+++ app/views/skeleton/header.php
@@ -12,6 +12,17 @@ function auto_version($file)
 	<head>	
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <?php if (!DEBUG): ?>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177328075-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-177328075-1');
+        </script>
+        <?php endif ?>
 		<link rel="apple-touch-icon" sizes="180x180" href="``PATH_PREFIX``/static/favicon/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="``PATH_PREFIX``/static/favicon/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="``PATH_PREFIX``/static/favicon/favicon-16x16.png">
